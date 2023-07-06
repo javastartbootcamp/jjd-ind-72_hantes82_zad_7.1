bc9a40e27eb2a6ed72b3e51f4baf55af40c10419
Konrad Putelbergier <Konrad.Putelbergier@gmail.com>
2023-07-06 08:46:01 +0200

    Zadanie 7.2

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -5,10 +5,16 @@ public class Main {
     public static void main(String[] args) {
         PalindromeService palindromeService = new PalindromeService();
 
-        int[] array = new int[]{1, 2, 3};
+        int[] array = new int[]{1, 2, 3, 2, 1};
 
         boolean isPalindrome = palindromeService.isPalindrome(array);
 
-        System.out.println(isPalindrome);
+        if (isPalindrome) {
+            System.out.println("Liczba powstała z połączenia cyfr w tablicy jest palindromem");
+        } else {
+            System.out.println("Liczba powstała z połączenia cyfr w tablicy nie jest palindromem");
+        }
     }
 }
+//0 = 4
+//1 = 3
--- src/main/java/pl/javastart/task/PalindromeService.java
+++ src/main/java/pl/javastart/task/PalindromeService.java
@@ -4,6 +4,14 @@ public class PalindromeService {
 
     // nie zmieniaj sygnatury tej metody. Jest ona testowana w PalindromeServiceTest
     public boolean isPalindrome(int[] array) {
-        return false;
+        if (array != null && array != new int[]{}) {
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] != array[array.length - 1 - i]) {
+                    return false;
+                }
+            }
+
+        }
+        return true;
     }
 }
